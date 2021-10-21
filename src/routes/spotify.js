be0d94ee947405d2a893a23c61370d4c6ed1cdec
DoceAzedo <oi@lucasfernandes.com.br>
2021-10-21 00:53:41 -0300

    🤨 ok a api do spotify que é estranha com ads

--- src/routes/spotify.js
+++ src/routes/spotify.js
@@ -10,7 +10,7 @@ export async function get() {
     }
   });
 
-  if (!resp.data?.is_playing) return {
+  if (!resp.data?.item) return {
     body: {
       title: 'Nada tocando :(',
       artist: '',
