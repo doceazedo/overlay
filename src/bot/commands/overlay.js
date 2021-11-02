ec35b72dbb439fe22abf73192ea03cb437a1e71a
DoceAzedo <oi@lucasfernandes.com.br>
2021-11-02 20:07:36 -0300

    🔗 Atualizar link do repositório do overlay

--- src/bot/commands/overlay.js
+++ src/bot/commands/overlay.js
@@ -1,5 +1,5 @@
 export const overlay = {
   exec: async (client, channel) => {
-    client.say(channel, `Código fonte do overlay: https://github.com/doceazedo/overlays`);
+    client.say(channel, `Código fonte do overlay: https://github.com/doceazedo/overlay`);
   }
 }
