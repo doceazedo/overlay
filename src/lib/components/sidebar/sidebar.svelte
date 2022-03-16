e40ac456dbc96775fc67e7bc31eee96e9596e87c
DoceAzedo <oi@lucasfernandes.com.br>
2022-03-15 22:05:19 -0300

    📏 Fix sidebar height

--- src/lib/components/sidebar/sidebar.svelte
+++ src/lib/components/sidebar/sidebar.svelte
@@ -2,23 +2,32 @@
   export let showHandcam = false;
 </script>
 
-<div class:handcam={showHandcam}>
-  <slot />
+<div class="sidebar-wrapper">
+  <div class="sidebar" class:handcam={showHandcam}>
+    <slot />
+  </div>
 </div>
 
 <style lang="sass">
-  div
+  .sidebar-wrapper
     position: absolute
-    bottom: 0
+    top: 0
     right: 0
     display: flex
     flex-direction: column
-    gap: 1rem
+    justify-content: flex-end
     width: 480px
-    height: 810px
+    height: 100%
+
+  .sidebar
+    display: flex
+    flex-direction: column
+    gap: 1rem
+    width: 100%
+    height: calc(100% - 270px)
     padding: 1rem
     background-color: #2d2d2d
 
     &.handcam
-      height: 540px
+      height: calc(100% - 540px)
 </style>
