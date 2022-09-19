5cda24e4f22b867815bf97176bd995eea1e6ab61
doceazedo <me@doceazedo.com>
2022-09-19 18:02:41 -0300

    Update start script (and default ports)

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-  plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  server: {
+    port: 2424
+  },
+  preview: {
+    port: 2425
+  }
 };
 
 export default config;
