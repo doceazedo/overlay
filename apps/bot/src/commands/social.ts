6f4e1c568345e51f8a33b93030a2447830aaa4a5
doceazedo <me@doceazedo.com>
2023-03-23 02:02:09 -0300

    Update social media links

--- apps/bot/src/commands/social.ts
+++ apps/bot/src/commands/social.ts
@@ -4,13 +4,13 @@ import type { Command } from '.';
 const socialsArr = [
   '🐙 GitHub: https://github.com/doceazedo',
   '🐤 Twitter: https://twitter.com/doceazedo911',
+  '🐘 Mastodon: https://svelte.gay/@doceazedo',
   '📸 Instagram: https://instagram.com/doceazedo911',
   '💬 Discord: https://discord.gg/vEGRe2kq8B',
   '🎵 Lastfm: https://last.fm/user/doceazedo911',
-  '💼 Polywork: https://polywork.com/doceazedo',
 ];
 
 export const social: Command = {
-  aliases: ['social', 'socials', 'redes', 'github', 'gh'],
+  aliases: ['social', 'socials', 'redes', 'github', 'gh', 'twitter', 'tt', 'mastodon', 'instagram', 'insta', 'ig', 'lastfm'],
   exec: async () => sendMultiple(socialsArr),
 };
