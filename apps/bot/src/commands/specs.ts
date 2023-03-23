75acd52ea6e557f7bd2366f3930471dc38ada0eb
doceazedo <me@doceazedo.com>
2023-03-22 22:41:20 -0300

    Update !specs

--- apps/bot/src/commands/specs.ts
+++ apps/bot/src/commands/specs.ts
@@ -1,26 +1,9 @@
-import { sendStriped } from '../utils';
+import { send } from '../utils';
 import type { Command } from '.';
 
-const specsArr = [
-  'CPU: AMD Ryzen 3600X',
-  'RAM: 24GB (3x8) Crucial 3000MHz',
-  'GPU: EVGA NVDIA GTX 1660',
-  'ROM: 2TB NVMe (Crucial) + 480GB SATA (Crucial) + 500GB HDD (WD)',
-  'Mobo: Gigabyte B450M DS3H',
-  'Case: Corsair Carbide Series SPEC-DELTA RGB',
-  'Displays: Samsung 24" 75hz 1ms + LG Flatron 17"',
-  'Teclado: Logitech G512',
-  'Mouse: Redragon Cobra',
-  'Mesa digitalizadora: Wacom CTL472',
-  'Headset: Logitech Prodigy G733',
-  'Microfone: HyperX QuadCast',
-  'Earbuds: Galaxy Buds+',
-  'Webcam: Logitech C922 Pro',
-];
-
 export const specs: Command = {
-  aliases: ['specs', 'spec', 'config', 'pc'],
+  aliases: ['specs', 'spec', 'config', 'pc', 'setup'],
   exec: async () => {
-    sendStriped(specsArr, 'DodgerBlue', 'BlueViolet');
+    send('Eu programo num MacBook Pro M1 14" 2021 🧑‍💻 e jogo num Ryzen 3600X com uma GTX 1660 🎮');
   },
 };
