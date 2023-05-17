6441c7f941da591a321febc2503e7c7d6dd0a963
doceazedo <me@doceazedo.com>
2023-05-16 23:42:09 -0300

    Update !specs

--- apps/bot/src/commands/specs.ts
+++ apps/bot/src/commands/specs.ts
@@ -4,6 +4,8 @@ import type { Command } from '.';
 export const specs: Command = {
   aliases: ['specs', 'spec', 'config', 'pc', 'setup'],
   exec: async () => {
-    send('Eu programo num MacBook Pro M1 14" 2021 🧑‍💻 e jogo num Ryzen 3600X com uma GTX 1660 🎮');
+    send(
+      'Eu programo num MacBook Pro M1 14" 2021 🧑‍💻 e jogo num Ryzen 3600X com uma GTX 1660 🎮 Mais detalhes em: https://doceazedo.com/uses'
+    );
   },
 };
