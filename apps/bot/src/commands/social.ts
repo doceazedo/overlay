c436a18035541bf3ff331c04d440ee11af27de28
doceazedo <me@doceazedo.com>
2023-05-16 23:41:37 -0300

    Update Mastodon

--- apps/bot/src/commands/social.ts
+++ apps/bot/src/commands/social.ts
@@ -4,13 +4,26 @@ import type { Command } from '.';
 const socialsArr = [
   '🐙 GitHub: https://github.com/doceazedo',
   '🐤 Twitter: https://twitter.com/doceazedo911',
-  '🐘 Mastodon: https://svelte.gay/@doceazedo',
+  '🐘 Mastodon: https://bolha.us/@doceazedo',
   '📸 Instagram: https://instagram.com/doceazedo911',
   '💬 Discord: https://discord.gg/vEGRe2kq8B',
   '🎵 Lastfm: https://last.fm/user/doceazedo911',
 ];
 
 export const social: Command = {
-  aliases: ['social', 'socials', 'redes', 'github', 'gh', 'twitter', 'tt', 'mastodon', 'instagram', 'insta', 'ig', 'lastfm'],
+  aliases: [
+    'social',
+    'socials',
+    'redes',
+    'github',
+    'gh',
+    'twitter',
+    'tt',
+    'mastodon',
+    'instagram',
+    'insta',
+    'ig',
+    'lastfm',
+  ],
   exec: async () => sendMultiple(socialsArr),
 };
