430cdb69bf62cc5e57fa6885b8a1ecfad7e4381f
doceazedo <me@doceazedo.com>
2023-05-30 21:52:08 -0300

    Listen for gift sub events

--- apps/bot/src/events/chat/index.ts
+++ apps/bot/src/events/chat/index.ts
@@ -1,5 +1,11 @@
 import { announcementEvent } from './announcement';
+import { giftSubEvent } from './gift-sub';
 import { resubEvent } from './resub';
 import { subEvent } from './sub';
 
-export const chatEvents = [announcementEvent, resubEvent, subEvent];
+export const chatEvents = [
+  announcementEvent,
+  giftSubEvent,
+  resubEvent,
+  subEvent,
+];
