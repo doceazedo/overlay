c8caea25b1709d7ef9cb7e98bb2a08ca6903b58d
doceazedo <me@doceazedo.com>
2023-06-01 21:55:29 -0300

    Update Twurple and other deps

--- apps/bot/src/event-handler.ts
+++ apps/bot/src/event-handler.ts
@@ -10,6 +10,26 @@ const eventHandler = async () => {
   const eventSubClient = await getEventSubClient();
   eventSubClient.start();
   loggr.init('EventSubWsListener started');
+  eventSubClient.onRevoke((x) => loggr.warn(`Subscription ${x.id} revoked`));
+  eventSubClient.onSubscriptionCreateFailure((x) =>
+    loggr.warn(`Could not subscribe for ${x.id}`)
+  );
+  eventSubClient.onSubscriptionCreateSuccess((x) =>
+    loggr.init(`Subscribed for ${x.id}`)
+  );
+  eventSubClient.onSubscriptionDeleteFailure((x) =>
+    loggr.warn(`Could not delete subscription for ${x.id}`)
+  );
+  eventSubClient.onSubscriptionDeleteSuccess((x) =>
+    loggr.init(`Subscription for ${x.id} deleted`)
+  );
+  eventSubClient.onUserSocketConnect((x) => loggr.debug('onUserSocketConnect'));
+  eventSubClient.onUserSocketDisconnect((id, error) => {
+    loggr.debug('onUserSocketDisconnect');
+    console.log(error?.message);
+    console.log(error?.name);
+    console.log(error?.stack);
+  });
   for (const event of twitchEvents) {
     event(eventSubClient, userId);
   }
