d875e1d084d414afbb3ee2465b5d162f6d03b992
doceazedo <me@doceazedo.com>
2023-06-21 01:57:47 -0300

    Add Footer

---
+++ apps/dashboard/src/lib/components/Footer.svelte
@@ -0,0 +1,15 @@
+<footer class="footer">
+	<div class="content has-text-centered">
+		<p>
+			<strong>Doce's Overlay V3</strong> by <a href="https://github.com/doceazedo">DoceAzedo</a>.
+			The source code is licensed
+			<a href="https://www.gnu.org/licenses/quick-guide-gplv3.html">GPLv3</a>.
+		</p>
+	</div>
+</footer>
+
+<style lang="scss">
+	.footer {
+		margin-top: auto;
+	}
+</style>
--- apps/dashboard/src/routes/+layout.svelte
+++ apps/dashboard/src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import 'bulma/css/bulma.min.css';
+	import '$lib/scss/global.scss';
 	import Navbar from '$lib/components/Navbar.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 
 	export let data;
 </script>
@@ -11,3 +13,4 @@
 		<slot />
 	</div>
 </main>
+<Footer />
