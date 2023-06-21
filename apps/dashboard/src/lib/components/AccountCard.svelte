a1ca5005e49780be5b585759c23efd2dddd87345
doceazedo <me@doceazedo.com>
2023-06-20 23:45:36 -0300

    Add AccountCard

---
+++ apps/dashboard/src/lib/components/AccountCard.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	type AccountData = {
+		name: string;
+		avatar: string;
+	};
+
+	export let label: string;
+	export let data: AccountData | null = null;
+	export let connectUrl: string;
+	export let disconnectUrl: string;
+</script>
+
+<div>
+	<h2>{label} account</h2>
+	{#if data}
+		<img src={data.avatar} alt="Avatar of {data.name}" />
+		<p>{data.name}</p>
+		<a href={connectUrl}>Disconnect</a>
+	{:else}
+		<p>Not connected!</p>
+		<a href={disconnectUrl}>Connect</a>
+	{/if}
+</div>
--- apps/dashboard/src/routes/accounts/+page.svelte
+++ apps/dashboard/src/routes/accounts/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import AccountCard from '$lib/components/AccountCard.svelte';
 
 	export let data;
 
@@ -9,6 +10,14 @@
 		`https://id.twitch.tv/oauth2/authorize?client_id=${data.twitchClientId}&redirect_uri=${redirectUri}/${type}&response_type=code&scope=chat:read+chat:edit`;
 
 	const getDisconnectUrl = (type: string) => `/accounts/remove/twitch/${type}`;
+
+	const getAccountData = (data: any) => {
+		if (!data) return null;
+		return {
+			name: data.displayName,
+			avatar: data.profilePictureUrl
+		};
+	};
 </script>
 
 <h1>Accounts</h1>
@@ -17,27 +26,16 @@
 	<div>Your Twitch application is not setup!</div>
 {/if}
 
-<ul>
-	<li>
-		<h2>Streamer account</h2>
-		{#if data.twitchBroadcasterUser}
-			<img src={data.twitchBroadcasterUser.profilePictureUrl} alt="" />
-			<p>{data.twitchBroadcasterUser.displayName}</p>
-			<a href={getDisconnectUrl('broadcaster')}>Disconnect</a>
-		{:else}
-			<p>Not connected!</p>
-			<a href={getOauthUrl('broadcaster')}>Connect</a>
-		{/if}
-	</li>
-	<li>
-		<h2>Bot account</h2>
-		{#if data.twitchBotUser}
-			<img src={data.twitchBotUser.profilePictureUrl} alt="" />
-			<p>{data.twitchBotUser.displayName}</p>
-			<a href={getDisconnectUrl('bot')}>Disconnect</a>
-		{:else}
-			<p>Not connected!</p>
-			<a href={getOauthUrl('bot')}>Connect</a>
-		{/if}
-	</li>
-</ul>
+<AccountCard
+	label="Streamer"
+	data={getAccountData(data.twitchBroadcasterUser)}
+	connectUrl={getOauthUrl('broadcaster')}
+	disconnectUrl={getDisconnectUrl('broadcaster')}
+/>
+
+<AccountCard
+	label="Bot"
+	data={getAccountData(data.twitchBotUser)}
+	connectUrl={getOauthUrl('bot')}
+	disconnectUrl={getDisconnectUrl('bot')}
+/>
