e5f9f34bb131a164a2b638222f8f90770918aab7
doceazedo <me@doceazedo.com>
2023-08-10 18:20:40 -0300

    Add !pronouns

---
+++ apps/bot/src/commands/pronouns.ts
@@ -0,0 +1,8 @@
+import { createBotReply } from "../commands";
+
+const pronouns = createBotReply(
+  ["pronouns", "pronoun", "pronomes", "pronome"],
+  "Meus pronomes são ele/delu! 💖"
+);
+
+export default pronouns;
