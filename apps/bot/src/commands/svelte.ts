50bf2b97c67dcb8a5448ce16a95b64db62c95bb3
doceazedo <me@doceazedo.com>
2023-08-10 18:19:02 -0300

    Add !svelte

---
+++ apps/bot/src/commands/svelte.ts
@@ -0,0 +1,8 @@
+import { createBotReply } from "../commands";
+
+const svelte = createBotReply(
+  ["svelte"],
+  "Tutorial de Svelte em Português 👉 https://doceazedo.com/blog/primeiros-passos-svelte"
+);
+
+export default svelte;
