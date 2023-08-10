73b1ea1c37ca6e70cb85ecee415a24030c446623
doceazedo <me@doceazedo.com>
2023-08-10 18:24:05 -0300

    Remove !dice and !slap

--- apps/bot/src/commands/dice.ts
+++
@@ -1,8 +0,0 @@
-import { createBotCommand } from "../commands";
-
-const dice = createBotCommand(["dice"], ({ reply }) => {
-  const diceRoll = Math.floor(Math.random() * 6) + 1;
-  reply(`You rolled a ${diceRoll}`);
-});
-
-export default dice;
--- apps/bot/src/commands/slap.ts
+++
@@ -1,9 +0,0 @@
-import { createBotCommand } from "../commands";
-
-const slap = createBotCommand(["slap"], (ctx) => {
-  ctx.say(
-    `${ctx.user} slaps ${ctx.params.join(" ")} around a bit with a large trout`
-  );
-});
-
-export default slap;
