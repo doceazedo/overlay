74d1a276d750003e0de68822d3db81b51f5c0c32
doceazedo <me@doceazedo.com>
2023-08-10 18:19:40 -0300

    Add !site

---
+++ apps/bot/src/commands/site.ts
@@ -0,0 +1,8 @@
+import { createBotReply } from "../commands";
+
+const site = createBotReply(
+  ["site", "website"],
+  "✨ Código fonte do meu site: https://github.com/doceazedo/doceazedo.com"
+);
+
+export default site;
