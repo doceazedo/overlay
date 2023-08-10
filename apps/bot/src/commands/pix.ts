9f0996cddfeacdb3b1d91e4d034b92cb95545f8a
doceazedo <me@doceazedo.com>
2023-08-10 18:23:41 -0300

    Add !pix

---
+++ apps/bot/src/commands/pix.ts
@@ -0,0 +1,8 @@
+import { createBotReply } from "../commands";
+
+const pix = createBotReply(
+  ["pix", "donate", "doar"],
+  "Minha chave Pix é: 4480693e-1273-4941-aacc-5840697aee42"
+);
+
+export default pix;
